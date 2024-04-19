49d0289d32c0e3fa2961891a7340c30b5bb3109c
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-19 00:30:40 -0300

    Websockets na prática - Página 404 e Desconexão do Cliente no Servidor

--- app.py
+++ app.py
@@ -64,9 +64,14 @@ def pix_confirmation():
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
   payment = Payment.query.get(payment_id)
+
+  if payment is None:
+    return render_template('404.html')
+
   if payment.paid:
     return render_template('confirmed_payment.html', payment_id=payment.id, 
                          value=payment.value)
+  
   return render_template('payment.html', 
                          payment_id=payment.id, 
                          value=payment.value, 
@@ -77,6 +82,10 @@ def payment_pix_page(payment_id):
 @socketio.on('connect')
 def handle_connect():
   print('Client connected to the server')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+  print('Client disconnected from the server')
 
 if __name__ == '__main__':
   socketio.run(app, debug=True)
