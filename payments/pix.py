998ccbf6a64331d3013ec28f9d89af60d591d2a5
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-19 00:44:00 -0300

    Testes e Conclusão - Criando Testes Unitários Para a Classe do Pix

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,7 @@ class Pix:
   def __init__(self) -> None:
     pass
 
-  def create_payment(self):
+  def create_payment(self, base_dir=''):
 
     #cria o pagamento de uma instituição financeira ficticia
     bank_payment_id = str(uuid.uuid4())
@@ -15,7 +15,7 @@ class Pix:
     #gera a imagem
     img = qrcode.make(hash_payment)
     #salva a imagem
-    img.save(f'static/img/qr_code_payment_{bank_payment_id}.png')
+    img.save(f'{base_dir}static/img/qr_code_payment_{bank_payment_id}.png')
 
     return {
       'bank_payment_id': bank_payment_id,
